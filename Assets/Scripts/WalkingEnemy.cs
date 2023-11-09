95250c63cd1815a74e09a1066dc8de9fd846305d
DwarfFromRoflandia <guckov5555@mail.ru>
2023-11-09 16:06:20 +0300

    Creating enemy behavior
    Chasing a player and stopping when attacking

--- Assets/Scripts/WalkingEnemy.cs
+++ Assets/Scripts/WalkingEnemy.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class WalkingEnemy : Enemy
 {
+    [SerializeField] private LayerMask _layerMask;
     private NavMeshAgent _navMeshAgent;
     private Transform _player;
     private float _attackRange;
@@ -22,12 +24,29 @@ public class WalkingEnemy : Enemy
     }
 
     private void Update()
+    { 
+        
+        if (IsVisibility())
+        {
+            //attack is called here
+            Debug.Log("Attacked");
+            _navMeshAgent.isStopped = true;
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    public bool IsVisibility()
     {
-        Move();
+        RaycastHit hit;
+        return Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, _layerMask);
     }
 
     public override void Move()
     {
+        _navMeshAgent.isStopped = false;
         _navMeshAgent.SetDestination(_player.position);
     }   
 }
